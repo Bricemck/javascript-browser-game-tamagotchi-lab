fa82cc5d17f3a7bc21a0c722e56f27db6e27ad94
Brice McKeel <Bricemck@gmail.com>
2025-06-27 21:54:43 -0400

    initiation timer interval

--- js/app.js
+++ js/app.js
@@ -1,34 +1,37 @@
 /*-------------------------------- Constants --------------------------------*/
+// const boredomStatEL = document.getElementById('boredom-stat');
+// const hungerStatEL = document.getElementById('hunger-stat');
+// const sleepinessStatEL= document.getElementById('sleepiness-stat'); 
+// const playButton = document.getElementById('play');
+// const feedButton = document.getElementById('feed');
+// const sleepButton = document.getElementById('sleep');
+// const resetBtnEl = document.getElementById('reset');
+// const gameMessageEl = document.getElementById('message');
+
 const state = {
     hunger: 0,
     boredom: 0,
     sleepiness: 0,
 };
 
-const boredomStatEL = document.getElementById('boredom-stat');
-const hungerStatEL = document.getElementById('hunger-stat');
-const sleepinessStatEL= document.getElementById('sleepiness-stat'); 
+let timer = 0;
+let gameOver = false;
 
-const playButton = document.getElementById('play');
-const feedButton = document.getElementById('feed');
-const sleepButton = document.getElementById('sleep');
+function runGame() {
+    console.log('the game is running!')
+}
 
-const resetBtnEl = document.getElementById('reset');
-const gameMessageEl = document.getElementById('message');
-
-const init = document.addEventListenter("DOMContentLoaded", function() {
-
-    console.log("App initialized after DOM is ready!");
-})
-
-let timer = {};
-
-
-
-let gameOver = () => {
-    console.log('game over!'),
-    process.exit()
+function init() {
+    timer = setInterval(runGame, 2000)
 };
+
+
+
+init();
+
+// const init = document.addEventListenter("DOMContentLoaded", function() {
+//     console.log("App initialized after DOM is ready!");
+// })
 
 
 
@@ -46,6 +49,13 @@ let gameOver = () => {
 
 /*---------------------------- Variables (state) ----------------------------*/
 
+
+
+
+// let gameOver = () => {
+//     console.log('game over!'),
+//     process.exit()
+// };
 
 
 /*------------------------ Cached Element References ------------------------*/
